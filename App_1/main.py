a5362d267276946be9410e131c92321854a55cad
David Loerinci <david.loerinci@gmail.com>
2021-10-14 12:03:38 +0200

    initial commit

---
+++ App_1/main.py
@@ -0,0 +1,13 @@
+import kivy
+from kivy.app import App
+from kivy.uix.button import Button
+
+
+class StartApp(App):
+    def build(self):
+        return Button(text="Python ist gigantisch")
+
+
+if __name__ == "__main__":
+    my_app = StartApp()
+    my_app.run()
